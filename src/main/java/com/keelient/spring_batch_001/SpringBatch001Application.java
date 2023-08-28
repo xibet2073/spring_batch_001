26115029368f846979da7fe289b33205f5d71ab4
xibet2073 <fosol2073@gmail.com>
2023-08-28 07:27:03 +0200

    first commit

---
+++ src/main/java/com/keelient/spring_batch_001/SpringBatch001Application.java
@@ -0,0 +1,13 @@
+package com.keelient.spring_batch_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch001Application.class, args);
+	}
+
+}
